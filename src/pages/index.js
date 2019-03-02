8707be382104bd69c81d86272d2536b12a201542
nakario <nakario.jp@gmail.com>
2019-03-03 00:10:35 +0900

    Set up very initial settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <p>Hello TDD!</p>
+    <p>
+      This website is developed in{" "}
+      <a href="https://github.com/nakario/test.driven.dev">this GitHub repo</a>.
+    </p>
+  </div>
+)
